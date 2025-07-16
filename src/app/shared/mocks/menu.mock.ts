f6edd3919d59e83371b2d153dbd3ad802ea61c81
Daniel <dannyking07@outlook.es>
2025-07-16 01:29:54 -0500

    feat(mock): agregar mock de menú con datos de ejemplo

---
+++ src/app/shared/mocks/menu.mock.ts
@@ -0,0 +1,7 @@
+import { MenuResponseEntity } from "@menu/core/entities/menu-response.entity";
+
+export const menuMock: MenuResponseEntity = {
+  menuId: 'menu123',
+  menuName: 'Main Menu',
+  menuItems: 'Item1, Item2, Item3'
+};
